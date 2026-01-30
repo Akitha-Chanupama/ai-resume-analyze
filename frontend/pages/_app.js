d0e755204ccc27bbe5e1ed3e2a876cc1ba3d8cbc
Akitha Chanupama <10899496@students.plymouth.ac.uk>
2026-01-30 08:08:11 +0530

    Rename MiApp to MyApp in _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 
-function MiApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system">
       <Component {...pageProps} />
